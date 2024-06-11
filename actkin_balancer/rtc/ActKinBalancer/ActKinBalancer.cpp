a839e8bd4ee9b46a678fe35ceb21a3f6a536f4c8
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-06-11 15:38:29 +0900

    writing

--- actkin_balancer/rtc/ActKinBalancer/ActKinBalancer.cpp
+++ actkin_balancer/rtc/ActKinBalancer/ActKinBalancer.cpp
@@ -39,59 +39,129 @@ RTC::ReturnCode_t ActKinBalancer::onInitialize(){
 }
 
 RTC::ReturnCode_t ActKinBalancer::onExecute(RTC::UniqueId ec_id){
+  std::lock_guard<std::mutex> guard(this->mutex_);
 
-  if (this->ports_.m_qRefIn_.isNew()){
-    this->ports_.m_qRefIn_.read();
+  this->loop_++;
+  std::string instance_name = std::string(this->m_profile.instance_name);
+  double rate = this->get_context(ec_id)->get_rate();
+  if(rate <= 0.0){
+    std::cerr << "\x1b[31m[" << instance_name << "] " << " periodic rate is invalid " << rate << "\x1b[39m" << std::endl;
+    return RTC::RTC_ERROR;
+  }
+  double dt = 1.0 / rate;
+
+  // 外部からのサービスコールによって指令されたモード変更を反映する
+  this->mode_.update();
+
+  // startABC直後の一回のみ実行
+  if(this->mode_.isSyncToABCInit()){
   }
 
-  if (this->ports_.m_tauRefIn_.isNew()){
-    this->ports_.m_tauRefIn_.read();
+  if(!ActKinBalancer::readInPortDataForState(this->ports_, instance_name, dt,
+                                               this->state_)){
+    return RTC::RTC_OK;  // qAct が届かなければ何もしない
   }
 
-  if (this->ports_.m_qActIn_.isNew()){
-    this->ports_.m_qActIn_.read();
+  if(this->mode_.isABCRunning()){
+    ActKinBalancer::readInPortDataForGoal(this->ports_, instance_name, dt, this->state_,
+                                            this->goal_);
   }
 
-  if (this->ports_.m_dqActIn_.isNew()){
-    this->ports_.m_dqActIn_.read();
-  }
-
-  if (this->ports_.m_tauActIn_.isNew()){
-    this->ports_.m_tauActIn_.read();
-  }
-
-  {
-    this->ports_.m_q_.tm = this->ports_.m_qRef_.tm;
-    this->ports_.m_q_.data.length(this->ports_.m_qRef_.data.length());
-    for (int i = 0 ; i < this->ports_.m_q_.data.length(); i++){
-      this->ports_.m_q_.data[i] = this->ports_.m_qRef_.data[i];
-    }
-    this->ports_.m_qOut_.write();
-  }
-
-  {
-    this->ports_.m_tau_.tm = this->ports_.m_qRef_.tm;
-    this->ports_.m_tau_.data.length(this->ports_.m_tauRef_.data.length());
-    for (int i = 0 ; i < this->ports_.m_q_.data.length(); i++){
-      this->ports_.m_tau_.data[i] = this->ports_.m_tauRef_.data[i];
-    }
-    this->ports_.m_tauOut_.write();
-  }
+  ActKinBalancer::writeOutPortData(this->state_, this->mode_,
+                                   this->ports_);
 
   return RTC::RTC_OK;
 }
 
+RTC::ReturnCode_t ActKinBalancer::onActivated(RTC::UniqueId ec_id){
+  std::lock_guard<std::mutex> guard(this->mutex_);
+  std::cerr << "[" << m_profile.instance_name << "] "<< "onActivated(" << ec_id << ")" << std::endl;
+  this->mode_.reset();
+  return RTC::RTC_OK;
+}
+RTC::ReturnCode_t ActKinBalancer::onDeactivated(RTC::UniqueId ec_id){
+  std::lock_guard<std::mutex> guard(this->mutex_);
+  std::cerr << "[" << m_profile.instance_name << "] "<< "onDeactivated(" << ec_id << ")" << std::endl;
+  return RTC::RTC_OK;
+}
+
+
+// static function
+bool ActKinBalancer::readInPortDataForState(ActKinBalancer::Ports& ports, const std::string& instance_name, const double& dt,
+                                            actkin_balancer::State& state){
+
+
+  if (ports.m_qRefIn_.isNew()){
+    ports.m_qRefIn_.read();
+  }
+
+  if (ports.m_tauRefIn_.isNew()){
+    ports.m_tauRefIn_.read();
+  }
+
+  if (ports.m_qActIn_.isNew()){
+    ports.m_qActIn_.read();
+  }
+
+  if (ports.m_dqActIn_.isNew()){
+    ports.m_dqActIn_.read();
+  }
+
+  if (ports.m_tauActIn_.isNew()){
+    ports.m_tauActIn_.read();
+  }
+
+  return true;
+}
+
+// static function
+bool ActKinBalancer::readInPortDataForGoal(ActKinBalancer::Ports& ports, const std::string& instance_name, const double& dt, const actkin_balancer::State& state,
+                                             actkin_balancer::Goal& goal){
+  return true;
+}
+
+// static function
+bool ActKinBalancer::writeOutPortData(const actkin_balancer::State& state, const ActKinBalancer::ControlMode& mode,
+                                        ActKinBalancer::Ports& ports){
+  {
+    ports.m_q_.tm = ports.m_qRef_.tm;
+    ports.m_q_.data.length(ports.m_qRef_.data.length());
+    for (int i = 0 ; i < ports.m_q_.data.length(); i++){
+      ports.m_q_.data[i] = ports.m_qRef_.data[i];
+    }
+    ports.m_qOut_.write();
+  }
+
+  {
+    ports.m_tau_.tm = ports.m_qRef_.tm;
+    ports.m_tau_.data.length(ports.m_tauRef_.data.length());
+    for (int i = 0 ; i < ports.m_q_.data.length(); i++){
+      ports.m_tau_.data[i] = ports.m_tauRef_.data[i];
+    }
+    ports.m_tauOut_.write();
+  }
+
+  return true;
+}
+
+
 bool ActKinBalancer::startBalancer(){
+  std::lock_guard<std::mutex> guard(this->mutex_);
+
   return true;
 }
 bool ActKinBalancer::stopBalancer(){
+  std::lock_guard<std::mutex> guard(this->mutex_);
+
   return true;
 }
 
 bool ActKinBalancer::setActKinBalancerParam(const actkin_balancer::ActKinBalancerService::ActKinBalancerParam& i_param){
+  std::lock_guard<std::mutex> guard(this->mutex_);
   return true;
 }
 bool ActKinBalancer::getActKinBalancerParam(actkin_balancer::ActKinBalancerService::ActKinBalancerParam& i_param){
+  std::lock_guard<std::mutex> guard(this->mutex_);
   return true;
 }
 
--- actkin_balancer/rtc/ActKinBalancer/ActKinBalancer.h
+++ actkin_balancer/rtc/ActKinBalancer/ActKinBalancer.h
@@ -8,6 +8,8 @@
 #include <rtm/CorbaPort.h>
 #include <rtm/idl/BasicDataType.hh>
 
+#include <mutex>
+
 #include <actkin_balancer_msgs/idl/ActKinBalancer.hh>
 
 #include "ActKinBalancerService_impl.h"
@@ -15,7 +17,25 @@
 #include "Goal.h"
 
 class ActKinBalancer : public RTC::DataFlowComponentBase{
+public:
+  ActKinBalancer(RTC::Manager* manager);
+  virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+  virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+  virtual RTC::ReturnCode_t onInitialize();
+  virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  bool startBalancer();
+  bool stopBalancer();
+
+  bool setActKinBalancerParam(const actkin_balancer::ActKinBalancerService::ActKinBalancerParam& i_param);
+  bool getActKinBalancerParam(actkin_balancer::ActKinBalancerService::ActKinBalancerParam& i_param);
+
 protected:
+  std::mutex mutex_;
+
+  unsigned long long loop_ = 0;
+  int debugLevel_ = 0;
+
   class Ports {
   public:
     Ports();
@@ -41,18 +61,53 @@ protected:
   };
   Ports ports_;
 
-public:
-  ActKinBalancer(RTC::Manager* manager);
-  virtual RTC::ReturnCode_t onInitialize();
-  virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+  class ControlMode{
+  public:
+    /*
+      MODE_IDLE -> startStabilizer() -> MODE_ST
+      MODE_ST -> stopStabilizer() -> MODE_IDLE
 
-  bool startBalancer();
-  bool stopBalancer();
+      MODE_IDLEの場合はstateのみ受け取り他は何もしない.
+      MODE_STの場合はgoalも受け取り、torqueを出力する.
+      切り替えの連続性を担保するにはこのRTCではない.
+     */
+    enum Mode_enum{ MODE_IDLE, MODE_ABC};
+    enum Transition_enum{ START_ABC, STOP_ABC};
+  private:
+    Mode_enum current, previous, next;
+  public:
+    ControlMode(){ reset(); }
+    void reset(){ current = previous = next = MODE_IDLE; }
+    bool setNextTransition(const Transition_enum request){
+      switch(request){
+      case START_ABC:
+        if(current == MODE_IDLE){ next = MODE_ABC; return true; }else{ return false; }
+      case STOP_ABC:
+        if(current == MODE_ABC){ next = MODE_IDLE; return true; }else{ return false; }
+      default:
+        return false;
+      }
+    }
+    void update(){
+      previous = current; current = next;
+    }
+    Mode_enum now() const{ return current; }
+    Mode_enum pre() const{ return previous; }
+    bool isSyncToABCInit() const{ return (current != previous) && (current==MODE_ABC);}
+    bool isSyncToIdleInit() const{ return (current != previous) && (current==MODE_IDLE);}
+    bool isABCRunning() const{ return (current==MODE_ABC) ;}
+  };
+  ControlMode mode_;
 
-  bool setActKinBalancerParam(const actkin_balancer::ActKinBalancerService::ActKinBalancerParam& i_param);
-  bool getActKinBalancerParam(actkin_balancer::ActKinBalancerService::ActKinBalancerParam& i_param);
-
-private:
+  actkin_balancer::State state_;
+  actkin_balancer::Goal goal_;
+protected:
+  static bool readInPortDataForState(ActKinBalancer::Ports& ports, const std::string& instance_name, const double& dt,
+                                     actkin_balancer::State& state);
+  static bool readInPortDataForGoal(ActKinBalancer::Ports& ports, const std::string& instance_name, const double& dt, const actkin_balancer::State& state,
+                                    actkin_balancer::Goal& goal);
+  static bool writeOutPortData(const actkin_balancer::State& state, const ActKinBalancer::ControlMode& mode,
+                               ActKinBalancer::Ports& ports);
 };
 
 extern "C"
